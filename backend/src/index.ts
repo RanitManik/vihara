c5af61f4ad9694c13a03399d7ed47e04ec44e12f
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-12-22 03:20:06 +0530

    added login route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import * as mongoose from "mongoose";
 import "dotenv/config";
 import userRoutes from "@/routes/users";
+import authRoutes from "@/routes/auth";
 
 mongoose.connect(process.env.MONGODB_URI as string);
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(4000, () => {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -20,18 +20,20 @@ const validateRegister = [
 router.post(
     "/register",
     validateRegister,
-    async (req: Request, res: Response) => {
+    async (req: Request, res: Response): Promise<void> => {
         // Validate the request and return errors if any
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ message: errors.array() });
+            res.status(400).json({ message: errors.array() });
+            return; // End function execution after sending response
         }
 
         try {
             let user = await User.findOne({ email: req.body.email });
 
             if (user) {
-                return res.status(400).json({ message: "User already exists" });
+                res.status(400).json({ message: "User already exists" });
+                return;
             }
 
             user = new User(req.body);
@@ -51,12 +53,10 @@ router.post(
                 maxAge: 86400000,
             });
 
-            return res
-                .status(201)
-                .json({ message: "User created successfully" });
+            res.status(201).json({ message: "User created successfully" });
         } catch (error) {
             console.error(error);
-            return res.status(400).send({ message: "Something went wrong" });
+            res.status(400).send({ message: "Something went wrong" });
         }
     },
 );
