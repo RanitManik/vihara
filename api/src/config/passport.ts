4feab0705105b8965b3c4d2f95de6123428011ab
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2026-04-13 01:02:28 +0530

    fix(passport): update callback URL to use backend environment variable

--- api/src/config/passport.ts
+++ api/src/config/passport.ts
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: env.GOOGLE_CLIENT_ID as string,
       clientSecret: env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: `${env.BACKEND_URL}/api/auth/google/callback`,
+      callbackURL: "/api/auth/google/callback",
+      proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
